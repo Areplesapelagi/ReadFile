c5dda6db360acbac14dcfc78ba738945aa094138
Ariff Hazmi <145521144+Areplesapelagi@users.noreply.github.com>
2024-08-07 21:58:18 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -57,7 +57,7 @@ async function loadAttendees() {
     const totalPaxElem = document.getElementById('total-pax');
 
     goingContainer.innerHTML = '';
-    goingContainer.innerHTML = '';
+    notgoingContainer.innerHTML = '';
    
     let totalGoing = 0;
     let totalNotGoing = 0;
